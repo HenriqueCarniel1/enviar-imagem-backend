c12b0f0647b2b8570fd50740779c5babc4ff2d63
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-07-27 13:00:55 -0300

    first commit

---
+++ servidor/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const porta = 4000;
+const storage = require('./multerConfig');
+const multer = require('multer');
+
+const upload = multer({storage: storage})
+
+app.use("/files", express.static("uploads"))
+
+app.post("/upload", upload.single('file'), (req,res) => {
+    return res.json(req.file.filename)
+})
+
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta http://localhost:${porta}`);
+});
